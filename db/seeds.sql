f08ae2fc3edbbec97f517380ca75b86753c0f9b3
Michelle Chou <chou.m.rdh@gmail.com>
2020-05-29 22:18:00 -0700

    updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 
 INSERT INTO burgers(burger_name, devoured)
-VALUES ("cheeseburger"),("hamburger"), ("spicy sriracha burger");
+VALUES ("cheeseburger", false),("hamburger", false), ("spicy sriracha burger", false);
